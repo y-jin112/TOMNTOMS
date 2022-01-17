66df82b7895b73f2a5dbfb7e6777b607faefffdf
YEJIN <robins2424@kakao.com>
2022-01-17 12:31:19 +0900

    new

--- js/common.js
+++ js/common.js
@@ -12,6 +12,14 @@ $(window).on('scroll',function(){
     });
 })
 
+$(window).on('scroll',function(){
+    $('.ani2').each(function(i){
+        if($(window).scrollTop() > $('.ani2').eq(i).offset().top - $(window).height()){
+            $('.ani2').eq(i).addClass('active');
+        }
+    });
+})
+
   
 
 const btnBurger = document.querySelector('.trigger-menu');
